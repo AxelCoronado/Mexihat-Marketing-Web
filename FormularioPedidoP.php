445b078e452b0ab24e46577b48d31b550cb5401e
AxelCoronado <124079149+AxelCoronado@users.noreply.github.com>
2023-11-26 20:52:43 -0600

    Update FormularioPedidoP.php

--- FormularioPedidoP.php
+++ FormularioPedidoP.php
@@ -31,8 +31,8 @@ $total=0;
 
 if (isset($_SESSION['user_id'])) {
 	$id = $_SESSION['user_id'];
-	$query = 'SELECT * FROM Cliente WHERE id_cliente=$id';
-	$query2 = 'SELECT * FROM Modelo WHERE id_modelo = $Mod';
+	$query = "SELECT * FROM Cliente WHERE id_cliente='$id'";
+	$query2 = "SELECT * FROM Modelo WHERE id_modelo ='$Mod'";
 	$stmt = $conn->prepare($query);
 	$stmt2 = $conn->prepare($query2);
 	$stmt->execute();
@@ -60,11 +60,11 @@ if (isset($_SESSION['user_id'])) {
 	$cantidad=0;
 	$idp = 0;
 	$idpi = 0;
-	$sql3 = 'SELECT MAX(id_pedido) FROM Pedido';
+	$sql3 = "SELECT MAX(id_pedido) FROM 'Pedido'";
 	$stmt3 = $conn->prepare($sql3);
 	$stmt3->execute();
 	$results3 = $stmt3->fetch(PDO::FETCH_ASSOC);
-	$idp = intval($results3['id_Pedido']);
+	$idp = intval($results3['id_pedido']);
 	$idpi = $idp + 1;
 	
 	if (!empty($_POST['cantidadPro_P']) && !empty($_POST['envio'])) {

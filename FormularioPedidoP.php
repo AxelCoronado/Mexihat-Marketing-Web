3c7691a4f97ff6a352d797067c10a902e1fe827d
AxelCoronado <124079149+AxelCoronado@users.noreply.github.com>
2023-11-26 20:58:15 -0600

    Update FormularioPedidoP.php

--- FormularioPedidoP.php
+++ FormularioPedidoP.php
@@ -2,12 +2,12 @@
 session_start();
 require_once 'databaseP.php';
 
-$Mod = '';
+$mod = '';
 if(!empty($_GET['vari'])){
-	$_SESSION['Mod']=$_GET['vari'];
-	$Mod = $_GET['vari'];
+	$_SESSION['mod']=$_GET['vari'];
+	$mod = $_GET['vari'];
 }else{
-	$Mod=$_SESSION['Mod'];
+	$mod=$_SESSION['mod'];
 }
 
 $id='';
@@ -32,7 +32,7 @@ $total=0;
 if (isset($_SESSION['user_id'])) {
 	$id = $_SESSION['user_id'];
 	$query = "SELECT * FROM Cliente WHERE id_cliente='$id'";
-	$query2 = "SELECT * FROM Modelo WHERE id_modelo ='$Mod'";
+	$query2 = "SELECT * FROM Modelo WHERE id_modelo ='$mod'";
 	$stmt = $conn->prepare($query);
 	$stmt2 = $conn->prepare($query2);
 	$stmt->execute();
@@ -76,7 +76,7 @@ if (isset($_SESSION['user_id'])) {
 	
 		$cantidad=$_POST['cantidadPro_P'];
 		$envio=$_POST['envio'];
-		$sql4 = "INSERT INTO Pedido (id_pedido, id_clienteP, id_modeloP, fecha_P, cantidadPro_P, total, alcance_P) VALUES ($idpi, '$id', '$Mod', '$fecha', '$cantidad', '$total', '$envio')";
+		$sql4 = "INSERT INTO Pedido (id_pedido, id_clienteP, id_modeloP, fecha_P, cantidadPro_P, total, alcance_P) VALUES ($idpi, '$id', '$mod', '$fecha', '$cantidad', '$total', '$envio')";
 		$stmt4 = $conn->prepare($sql4);
 		if ($stmt4->execute()) {
 			echo "<script>
@@ -110,7 +110,7 @@ if (isset($_SESSION['user_id'])) {
 				<input type="text" value="<?php echo $correo; ?>" placeholder="Ingrese su correo">
 
 				<label for="serie">Serie Modelo</label>
-				<input type="text" value="<?php echo $Mod; ?>" placeholder="">
+				<input type="text" value="<?php echo $mod; ?>" placeholder="">
 
 				<label for="modelo">Modelo</label>
 				<input type="text" value="<?php echo $nomMode; ?>" placeholder="">
